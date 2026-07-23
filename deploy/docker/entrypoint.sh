51085faaf4027cce30531acda6116f44316c0a71
BharathiGaneshkumar <bharathi1010ad@gmail.com>
2026-07-23 09:57:49 +0000

    Add Kubernetes StatefulSet manifest for coordinators, entrypoint script for pod-index-based node ID assignment

---
+++ deploy/docker/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+POD_NAME=$(hostname)
+POD_INDEX=$(echo "$POD_NAME" | grep -o '[0-9]*$')
+NODE_ID=$((POD_INDEX + 1))
+
+PEERS=""
+for i in 0 1 2; do
+	if [ "$i" != "$POD_INDEX" ]; then
+		PEER_ID=$((i + 1))
+		if [ -n "$PEERS" ]; then
+			PEERS="$PEERS,"
+		fi
+		PEERS="$PEERS$PEER_ID=coordinator-$i.coordinator:50051"
+	fi
+done
+
+exec /app/coordinator -id="$NODE_ID" -port=50051 -peers="$PEERS" -clustersize=3
